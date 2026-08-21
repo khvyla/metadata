6cdf327acb437b68a9a1efc61d96fc162b6516af
DAXDigital <info@daxdigi.com>
2026-08-21 15:53:24 +0300

    feat: add metadata resolver foundation

--- src/index.ts
+++ src/index.ts
@@ -2,5 +2,7 @@ export { detectMetadata } from "./core/detect";
 export { normalizeMetadata } from "./core/normalize";
 export { convertMetadata, parseMetadata, processMetadata, registerParser } from "./core/engine";
 export { isStreamMetadataError, readStreamMetadata } from "./transport/icy";
+export { resolveMetadata } from "./resolver";
 export type { BuiltInMetadataFormat, CanonicalMetadata, Detection, MetadataFormat, MetadataParser, ParseOptions, ProcessOptions } from "./core/types";
 export type { StreamMetadata, StreamMetadataError, StreamMetadataErrorCategory, StreamMetadataOptions, StreamMetadataResult, StreamTransportHeaders } from "./transport/icy";
+export type { BuiltInResolutionMethod, ResolutionAttempt, ResolutionMethod, ResolveMetadataOptions, ResolveMetadataResult, ResolvedMetadata, SidecarSource, UnresolvedMetadata } from "./resolver";
